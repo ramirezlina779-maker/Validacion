a2be3c817c7213a85bcc55d3f419824517e4b33f
Lina Ramirez <ramirezlina779@gmail.com>
2026-02-17 11:22:01 -0500

    Contraseña

--- validacion.js
+++ validacion.js
@@ -1,5 +1,6 @@
 let usuario = document.getElementById("usuario")
 let mensaje = document.getElementById("mensaje")
+let password = document.getElementById("password");
 
 usuario.addEventListener("input", function (e) {
     let valor = this.value
@@ -21,4 +22,25 @@ usuario.addEventListener("input", function (e) {
     }
 
     this.value = valor.replace(/[^a-zA-Z]/g, '')
+})
+
+
+let mensajePassword = document.getElementById("mensajePassword");
+
+password.addEventListener("input", function () {
+
+    let valor = password.value
+    let longitud = valor.length
+
+    if (longitud > 10) {
+        mensajePassword.textContent = "Contraseña válida ✅"
+        mensajePassword.style.color = "green"
+        password.style.border = "2px solid green"
+    } else {
+        let faltan = 10 - longitud
+        mensajePassword.textContent = "Faltan " + faltan + " caracteres"
+        mensajePassword.style.color = "red"
+        password.style.border = "2px solid red"
+    }
+
 })
